373e881a6fb42d93dc6f1e7ba485f33a9c1826d6
Rebecca Lee <rlee212@github.com>
2025-07-06 16:51:46 -0400

    first commit

---
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -0,0 +1,118 @@
+import React from 'react';
+
+import Button from '../Button';
+
+import styles from './ToastPlayground.module.css';
+
+import Toast from '../Toast';
+import ToastShelf from '../ToastShelf';
+
+const VARIANT_OPTIONS = ['notice', 'warning', 'success', 'error'];
+
+function ToastPlayground() {
+
+  const [variant, setVariant] = React.useState(
+    VARIANT_OPTIONS[0]
+  );
+  const [message, setMessage] = React.useState("");
+  const [id, setId] = React.useState("");
+  const [toastList, setToastList] = React.useState([]);
+
+  function handleDismiss(id) {
+    const newList = toastList.filter((toast) => {
+      return toast.id !== id;
+    });
+
+    setToastList(newList);
+  }
+
+  const generateRandomId = (length = 6) => {
+    return Math.random().toString(36).substring(2, length + 2);
+  };
+
+  function handleNewToast(event) {
+    event.preventDefault();
+
+
+    const newList = [
+      ...toastList, {
+        id,
+        message,
+        variant
+      }
+    ]
+    setToastList(newList)
+    setMessage('');
+    setVariant(VARIANT_OPTIONS[0]);
+    setId('')
+  }
+
+  return (
+    <div className={styles.wrapper}>
+      <header>
+        <img alt="Cute toast mascot" src="/toast.png" />
+        <h1>Toast Playground</h1>
+      </header>
+
+
+      <ToastShelf
+        toastList={toastList}
+        variant={variant}
+        handleDismiss={handleDismiss}
+      >
+      </ToastShelf>
+
+      <form className={styles.controlsWrapper} onSubmit={handleNewToast}>
+        <div className={styles.row}>
+          <label
+            htmlFor="message"
+            className={styles.label}
+            style={{ alignSelf: 'baseline' }}
+          >
+            Message
+          </label>
+          <div className={styles.inputWrapper}>
+            <textarea id="message" className={styles.messageInput} value={message} onChange={(event) => { setMessage(event.target.value) }} />
+          </div>
+        </div>
+
+        <div className={styles.row}>
+          <div className={styles.label}>Variant</div>
+          <div
+            className={`${styles.inputWrapper} ${styles.radioWrapper}`}
+          >
+            {VARIANT_OPTIONS.map((option) => {
+              const id = `variant-${option}`;
+              return (
+                <label key={id} htmlFor={id}>
+                  <input
+                    id={id}
+                    type="radio"
+                    name="variant"
+                    value={option}
+                    checked={option === variant}
+                    onChange={(event) => {
+                      setVariant(event.target.value);
+                    }}
+                  />
+                  {option}
+                </label>
+              );
+            })}
+          </div>
+        </div>
+
+        <div className={styles.row}>
+          <div className={styles.label} />
+          <div
+            className={`${styles.inputWrapper} ${styles.radioWrapper}`}
+          >
+            <Button onClick ={()=>setId(generateRandomId(10))}>Pop Toast!</Button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default ToastPlayground;
---
+++ src/components/ToastShelf/ToastShelf.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import Toast from '../Toast';
+import styles from './ToastShelf.module.css';
+
+function ToastShelf({toastList, handleDismiss}) {
+
+  return (
+    <ol className={styles.wrapper}>
+      {toastList.map((toast) => (
+        <li key={toast.id} className={styles.toastWrapper}>
+          <Toast
+            id={toast.id}
+            variant={toast.variant}
+            handleDismiss={handleDismiss}
+          >
+            {toast.message}
+          </Toast>
+        </li>
+      ))}
+    </ol>
+  );
+}
+
+export default ToastShelf;
